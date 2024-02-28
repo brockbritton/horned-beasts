d365852d81a99a5188f1f0dcc86acd1927177e4e
Brock Britton <brockbritton@outlook.com>
2024-02-27 20:06:02 -0800

    bootstrap styles added

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,17 +1,28 @@
 
 import HornedBeast from "./HornedBeast";
 import beastData from "./assets/data.json"
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Gallery() {
-
-    return (
-        <>
-        {beastData.map(obj => {
-            return <HornedBeast key={obj._id} title={obj.title} description={obj.description} imageUrl={obj.image_url} />
-        })}
-    
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            {beastData.map(obj => {
+              return (
+                <HornedBeast 
+                key={obj._id}
+                title={obj.title} 
+                description={obj.description} 
+                imageUrl={obj.image_url} />  
+              )
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Gallery
--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,12 +1,16 @@
-
+import { useState } from 'react';
+import { Card } from 'react-bootstrap';
 
 function HornedBeast(props) {
-
+    const [likes, setLikes] = useState(0);
     return (
         <>
-            <h2> {props.title} </h2>
-            <img src={props.imageUrl} alt="" />
-            <p> {props.description} </p>
+            <Card.Img style={{width:"50%"}} onClick={() => setLikes(likes + 1)} src={props.imageUrl} alt="" />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text> {props.description} </Card.Text>
+                <Card.Text> Favorites: {likes} </Card.Text>
+            </Card.Body>
         </>
     )
 }
